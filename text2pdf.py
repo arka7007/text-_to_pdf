3e8be6eec189d8b1a37d4da58a8d566102b95043
arka7007 <46426501+arka7007@users.noreply.github.com>
2019-04-03 13:55:18 +0530

    Update text2pdf.py

--- text2pdf.py
+++ text2pdf.py
@@ -20,22 +20,12 @@ def text_to_pdf(text, font, size, out_file):
 			* return1: check
 				* type: bool
 	"""  
-
-
 	pdf = utl.create_instance()
 	pdf = utl.add_new_page(pdf)
 	pdf = utl.set_font_size(pdf, font, size)
 	pdf = utl.page_config(text,pdf)
 	check = utl.save_pdf(out_file, pdf)
 	return check
-
-
-# if __name__ == '__main__':
-# 	text = "Hope it works"
-# 	font = "Arial"
-# 	size = 12
-# 	out = "1.pdf"
-# 	text_to_pdf(text, font, size,out)
 
 
 
